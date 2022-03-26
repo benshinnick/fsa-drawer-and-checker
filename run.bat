10aa9e42923859ef4c78f400827a3356f41daaa5
Ben Shinnick <shinnickbenjamin@gmail.com>
2022-03-25 20:22:48 -0500

    Added batch file

---
+++ run.bat
@@ -0,0 +1,4 @@
+python3 src/fsa.py input-files/fsa1.txt input-files/legal.txt
+python3 src/fsa.py input-files/fsa1.txt input-files/illegal.txt
+python3 src/fsa.py input-files/fsa2.txt input-files/legal.txt
+python3 src/fsa.py input-files/fsa2.txt input-files/illegal.txt
