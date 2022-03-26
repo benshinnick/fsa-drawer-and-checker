737faa373c50ca514d47bb9e5f03084f07c8f9aa
Ben Shinnick <shinnickbenjamin@gmail.com>
2022-03-25 20:31:07 -0500

    Arranged Files For Project Submission

--- run.bat
+++ run.bat
@@ -1,4 +1,2 @@
-python3 src/fsa.py input-files/fsa1.txt input-files/legal.txt
-python3 src/fsa.py input-files/fsa1.txt input-files/illegal.txt
-python3 src/fsa.py input-files/fsa2.txt input-files/legal.txt
-python3 src/fsa.py input-files/fsa2.txt input-files/illegal.txt
+python3 fsa.py fsa.txt legal1.txt
+python3 fsa.py fsa.txt illegal1.txt
